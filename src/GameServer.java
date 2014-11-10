1ad3915e6b1004c37384bc19a455fdf28e24c039
ronaldiscool <ronaldiscool@gmail.com>
2014-11-09 22:13:13 -0800

    ,

--- src/GameServer.java
+++ src/GameServer.java
@@ -67,7 +67,6 @@ public class GameServer extends JFrame{
 				String[] namel = concatNames.split("\n");
 				for(String n:namel)
 				{
-					System.out.println("TEST"+n);
 					JLabel jl = new JLabel(n);
 					GameServer.setup.playerPanel.add(jl);
 					setup.revalidate();
